0b8554344824e3c648e5b8739bf653ac964eeb49
pingshian0131 <pingshian0131@gmail.com>
2025-06-30 08:56:25 +0800

    add db init bash

--- run_db.bash
+++ run_db.bash
@@ -1,5 +1,5 @@
 docker run -d -p 8086:8086 \
   -v "$PWD/data:/var/lib/influxdb2" \
   -v "$PWD/config:/etc/influxdb2" \
-  --env-file /home/ping-pi5/.env/influxdb.env \
+  --env-file ~/.env/influxdb.env \
   influxdb:latest
